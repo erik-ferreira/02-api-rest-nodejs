2db7c372a62ad4761375318d5334672c0404ef26
erik-ferreira <erikdesouzagf@gmail.com>
2025-11-17 23:51:24 -0300

    start config knex with db

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify"
+
+import { knex } from "./database.js"
+
+const app = fastify()
+
+app.get("/hello", async () => {
+  const tables = knex("sqlite_schema").select("*")
+  return tables
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running on http://localhost:3333")
+})

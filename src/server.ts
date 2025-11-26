39c585f30f709e227f8c120d6dda707e9bd4f062
Erik Ferreira <erikdesouzagf@gmail.com>
2025-11-26 00:13:55 -0300

    add route to create transaction

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import fastify from "fastify"
-import crypto from "node:crypto"
 
-import { knex } from "./database.js"
+import { transactionsRoutes } from "./routes/transactions.js"
 
 import { env } from "./env/index.js"
 
 const app = fastify()
 
-app.get("/hello", async () => {
-  const transactions = knex("transactions").select("*")
-
-  return transactions
-})
+app.register(transactionsRoutes, { prefix: "transactions" })
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP server running on http://localhost:${env.PORT}`)

eb34e61db4b2b4b875bcd6d57c105cdc4a05fba0
erik-ferreira <erikdesouzagf@gmail.com>
2025-11-25 23:20:05 -0300

    valid environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import fastify from "fastify"
+import crypto from "node:crypto"
 
 import { knex } from "./database.js"
+
+import { env } from "./env/index.js"
 
 const app = fastify()
 
 app.get("/hello", async () => {
-  const tables = knex("sqlite_schema").select("*")
-  return tables
+  const transactions = knex("transactions").select("*")
+
+  return transactions
 })
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running on http://localhost:3333")
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP server running on http://localhost:${env.PORT}`)
 })

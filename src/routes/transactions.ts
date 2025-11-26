275b1fd770849abb9adac11a59947a9779c549a4
erik-ferreira <erikdesouzagf@gmail.com>
2025-11-26 00:50:51 -0300

    route to list and get one transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,31 @@
-import z from "zod"
+import z, { string } from "zod"
 import crypto from "node:crypto"
 import { FastifyInstance } from "fastify"
 
 import { knex } from "../database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select("*")
+
+    return { transactions }
+  })
+
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex("transactions")
+      .where("id", id)
+      .select("*")
+      .first()
+
+    return { transaction }
+  })
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),

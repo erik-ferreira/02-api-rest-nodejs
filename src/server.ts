134a16015289961e58886df3fb43d90208569874
erik-ferreira <erikdesouzagf@gmail.com>
2025-11-27 23:38:54 -0300

    create first teste with server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
-import fastify from "fastify"
-import cookie from "@fastify/cookie"
-
-import { transactionsRoutes } from "./routes/transactions.js"
-
-import { env } from "./env/index.js"
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, { prefix: "transactions" })
+import { app } from "./app"
+import { env } from "./env"
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP server running on http://localhost:${env.PORT}`)
